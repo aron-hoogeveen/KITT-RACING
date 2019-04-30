323d2bb46c31ebc52b361998dc0e19d5a72613a5
Aron Hoogeveen <kahoogeveen@tudelft.net>
2019-04-30 10:19:56 +0200

    Update for operateKITT.m which adds error checking and a stress strop

---
+++ matlab/delayMeasurement.m
@@ -0,0 +1,28 @@
+function [timeAverage] = delayMeasurement(comPort)
+%USEFUL COMMENT
+
+% Open connection
+status = EPOCommunications('open', comPort);
+
+if (status == 0)
+    disp('Connection not established');
+else
+    disp('Connection established');
+end
+
+% Enter execution loop
+n = 100;
+% Start time measurement
+tic
+    for i=1:n
+        EPOCommunications('transmit', 'M150');
+        status = EPOCommunications('transmit', 'S');
+    end
+toc
+elapsedTime = toc;
+
+disp(elapsedTime/n);
+
+% Close the connection
+EPOCommunications('close');
+end
