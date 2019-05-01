95f43d234430577ec5d95fffb3af84401625ea3f
Aron Hoogeveen <aron.hoogeveen@gmail.com>
2019-05-01 10:27:27 +0200

    Moved files to matlab subdirectoy

--- KITTVelocity.m
+++
@@ -1,4 +0,0 @@
-plot(yout.time,yout.signals.values)
-ylabel('v (m/s)');
-xlabel('t (s)');
-title('Speed of the car in time');
---
+++ matlab/KITTVelocity.m
@@ -0,0 +1,4 @@
+plot(yout.time,yout.signals.values)
+ylabel('v (m/s)');
+xlabel('t (s)');
+title('Speed of the car in time');
