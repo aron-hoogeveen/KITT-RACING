220c8b23e4b8fc0ab29220469e2fee351b65fb39
Aron Hoogeveen <kahoogeveen@tudelft.net>
2019-05-02 11:26:39 +0200

    Update for mainKITT.m
    which adds a function help description;
    which adds more usefull comments to the file;
    which changes the way the distance is requested from the KITT racing
    car. This is yet not fully implemented;
    which adds useful error feedback for the user.

--- matlab/mainKITT.m
+++ matlab/mainKITT.m
@@ -1,11 +1,46 @@
 function [distanceR, distanceL] = mainKITT(comPort, speed, stopDistance)
-%MAINKITT control unit for challenge 1
-%    speed is defined as: 
+%mainKITT controls the KITT racing car.
+%    [distanceR, distanceL] = mainKITT(comPort, speed, stopDistance) opens
+%    up a serial bluetooth connection with the KITT racing car, with which
+%    commands the car to drive forward with a predefined speed. It reads 
+%    out sensor data from the car. Together with the stopDistance it 
+%    determines wether to send the stop signal to the car or not.
+%    
+%    Notes: 
+%    The speed of the car is of the format 'xxx', where xxx denotes a 
+%    value between and including 156 and 160. 
+%
+%    This function uses Windows specific functions
+%    (EPOCommunications.mexw64) so it can only be used in a Windows
+%    environment.
+%
+%
+%    (c) 2019 Epo-4 Group B.04 "KITT Racing"
+
+% Error strings
+strSpeedErr = '"speed" needs to be of the form "xxx" with xxx an integer value between 156 and 160.';
+
+% Default values
+cmdRefreshTime = 0.09; % [s] FIXME: fine tune this interval
+
+% Check input arguments
 if (nargin < 2)
-    error('Not enough input arguments (two required).');
+    error('Not enough input arguments (minimum of two required). Type "help mainKITT" to check the syntax of this function.');
 end
 if (nargin < 3)
-    stopDistance = 100;
+    stopDistance = 100; % Default stop distance.
+end
+
+% Check format of speed
+if (length(speed) ~= 3)
+    error(strSpeedErr);
+elseif (str2num(speed) < 156 || str2num(speed) > 160)
+    error('The speeds needs to be between 156 and 160.');
+end
+
+% Check format of comPort
+if (comPort(1:3) ~= 'COM')
+    error('comPort needs to be of the form "COMx", with x the comport number');
 end
 
 try
@@ -22,7 +57,8 @@ try
     % functions. KITT_STOP signals that the smooth_stop function should be
     % activated. distL and distR contain the current read out sensor values
     % TODO: read out a couple of sensor values and take the average. This
-    % can be done to filter out sensor errors. 
+    % can be done to filter out sensor errors. Consider the amount of time
+    % it takes to take multiple measurements.
     global KITT_STOP;
     global distR;
     global distL;
@@ -37,26 +73,28 @@ try
     
     % Wait for user input to start
     input('Press enter to start...');
+    disp('READY');
+    pause(0.5); % This is worth our valuable time
+    disp('SET');
+    pause(0.4);
     
     % Start driving forward
     speedKITT = strcat('M', speed);
     EPOCommunications('transmit', 'D154');  % Fix the steering offset of the car
+    % TODO: save default values in a seperate file and read them out. This
+    % way it is possible to change a specific value in one place only and
+    % have the effect through all functions.
     EPOCommunications('transmit', speedKITT);
     
-    disp('Process started.');
+    disp('GO!');
     while (KITT_STOP ~= 1)
-        % Main control loop
-        % Start met rechtdoor rijden
-        % Status continue opslaan
-        %   Extract distance
-        % Check for stop signal
-        %   Stop the car + status
+        % TODO: Request only the distance status 'Sd'
         
-        status = EPOCommunications('transmit', 'S');
+        status = EPOCommunications('transmit', 'Sd'); % Request only distance
         % Extract sensor values from the returned status
-        distIndex = strfind(status, 'Dist');
-        distEnd = strfind(status(distIndex:end), '*');
-        distStr = status(distIndex:distIndex+distEnd-3);
+%         distIndex = strfind(status, 'Dist');
+%         distEnd = strfind(status(distIndex:end), '*');
+%         distStr = status(distIndex:distIndex+distEnd-3);
         distStr = transpose(split(distStr));
         distRtemp = distStr(3);
         distR = str2num(distRtemp{1});
@@ -67,16 +105,22 @@ try
         distanceR = [distanceR, distR];
         distanceL = [distanceL, distL];
         
+        % TODO: implement the stop curve in a way that makes the car stop
+        % at exactly the stopDistance. At this moment the car starts
+        % stopping at the stopDistance.
         if (distR < stopDistance || distL < stopDistance)
             KITT_STOP = 1;
+            % FIXME: consider using break instead of KITT_STOP. This will
+            % only make sense when only this conditional statement
+            % determines the moment to stop.
         end
         
-        pause(0.09); % Command refresh time
+        pause(cmdRefreshTime);
     end
     
     % Close the connection
     EPOCommunications('close');
-    disp('Done with execution');
+    disp('Done with execution, we are.');
 catch e
     % MAIDAY MAIDAY CLOSE THE CONNECTION
     EPOCommunications('close');
