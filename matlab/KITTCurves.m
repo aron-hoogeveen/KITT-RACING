a866df3b02a0d86743e2bc9d282b7709bb459372
RobertB01 <35612511+RobertB01@users.noreply.github.com>
2019-05-06 22:17:31 +0200

    Plot van lijnen

---
+++ matlab/KITTCurves.m
@@ -0,0 +1,11 @@
+% Plot the curves
+
+for i = 1:9
+    plot(x_acc(i, :), v_acc(i, :));
+    hold on;
+end
+plot(x_roll160, v_roll160);
+hold on;
+plot(x_brake160, v_brake160, '--');
+plot(x_brake165, v_brake165, '--');
+legend("M:157, 19V", "M:158, 19V", "M159, 19V", "M160, 18.9V", "M161, 18.9V", "M162, 18.9V", "M163, 18.8V", "M164, 18.8V", "M165, 18.8V", "Rollout (M:160)", "Brake(M:160, 18.5V)", "Brake(M:165, 18.5V)");
