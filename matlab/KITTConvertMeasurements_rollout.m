5f3cf92d3e50e8d2f2f0eb41663f2943d7dc2cbe
RobertB01 <35612511+RobertB01@users.noreply.github.com>
2019-05-06 21:23:52 +0200

    Brake en roll curves gemaakt

--- matlab/KITTConvertMeasurements_rollout.m
+++ matlab/KITTConvertMeasurements_rollout.m
@@ -1,7 +1,8 @@
-% Converts measurements to matrices of brake with battery voltage:   18.4-18.6V
+% Converts measurements to matrices of rollout with battery voltage:
+% 18.4-18.6V
 
-for i = 1:length(stopL)
-    distComb(i) = (stopL(i) + stopR(i))/2;
+for i = 1:length(rollL)
+    distComb(i) = (rollL(i) + rollR(i))/2;
 end
 
 %Flip the distance (as lowering of sensorvalues indicates more distance
@@ -15,10 +16,10 @@ Vcomb = diff(Xcomb);
 Vcomb(length(Xcomb)) = Vcomb(length(Xcomb)-1);
 
 %Save brake curve
-x_brake165 = Xcomb;
-v_brake165 = Vcomb;
+x_roll158 = Xcomb;
+v_roll158 = Vcomb;
 
 % Determine rollout end point for shifting
-x_brake165_end = 120;
+x_roll158_end = 93;
 
-plot(x_brake165, v_brake165);
+plot(x_roll158, v_roll158);
