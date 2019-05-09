3cfa5e2f7c9fedb3fab40ab1725262375e0101e6
RobertB01 <35612511+RobertB01@users.noreply.github.com>
2019-05-09 21:42:09 +0200

    Stashen lukte niet

--- matlab/KITTConvertMeasurements_brake.m
+++ matlab/KITTConvertMeasurements_brake.m
@@ -1,5 +1,5 @@
 % Converts measurements to matrices of brake with battery voltage:   18.4-18.6V
-
+clear distComb;
 for i = 1:length(stopL)
     distComb(i) = (stopL(i) + stopR(i))/2;
 end
@@ -9,16 +9,16 @@ end
 Xcomb = distComb(1)-distComb;
 
 % smooth Xcomb data:
-Xcomb = smooth(smooth(Xcomb));
+Xcomb = smooth(smooth(smooth(smooth(Xcomb))));
         
 Vcomb = diff(Xcomb);
 Vcomb(length(Xcomb)) = Vcomb(length(Xcomb)-1);
 
 %Save brake curve
-x_brake165 = Xcomb;
-v_brake165 = Vcomb;
+x_brake160 = Xcomb;
+v_brake160 = Vcomb;
 
 % Determine rollout end point for shifting
-x_brake165_end = 120;
+x_brake160_end = 225;
 
-plot(x_brake165, v_brake165);
+plot(x_brake160, v_brake160);
