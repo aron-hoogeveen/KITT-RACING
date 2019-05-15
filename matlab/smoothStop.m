ffb8644202b77e29c08ecefeee65050af4c5a9cc
Aron Hoogeveen <aron.hoogeveen@gmail.com>
2019-05-15 11:39:11 +0200

    Adds a panic brake to the otherwise case

--- matlab/smoothStop.m
+++ matlab/smoothStop.m
@@ -1,9 +1,7 @@
 function [] = smoothStop(speed)
-%smoothStop(startSpeed) aims to stop the KITT Racing car without overshoot
-%    when the car has a constant speed of startSpeed.
-%
-%    TODO: Test if it is needed to use the realtime speed as input instead
-%    of startSpeed, since maybe the car is not yet at full speed.
+%smoothStop(speed) stops the KITT Racing car that is driving at a constant
+%    speed with speed setting 'speed'.
+
 switch speed
     case 157
         stopSpeed = 'M145';
@@ -33,9 +31,14 @@ switch speed
         pause(stopTime);
         EPOCommunications('transmit', 'M150');
     otherwise
-        disp('The provided speed for the smoothStop function could not be found in the switch statement.');
+        warning('smoothStop: the provided speed setting could not be processed. Trying to slow down the car now...');
+        % Brake for a short time to avoid major crashes.
+        EPOCommunications('transmit', 'M140');
+        pause(0.3);
+        EPOCommunications('transmit', 'M150');
         disp('Closing the connection te the car...');
         EPOCommunications('close');
+        disp('Connection closed.');
 end%switch
 
 end%smoothStop
