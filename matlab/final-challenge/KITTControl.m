09dbfa6c958c0c62942462c901aa69f617d163e4
RobertB01 <35612511+RobertB01@users.noreply.github.com>
2019-05-27 15:54:52 +0200

    added args

--- matlab/final-challenge/KITTControl.m
+++ matlab/final-challenge/KITTControl.m
@@ -1,8 +1,16 @@
-function [] = KITTControl(argsIn)
+function [] = KITTControl(startpoint, endpoint, waypoint, obstacles)
 %[] = KITTControl(argsIn) is the main control unit for the final challenge
-%    for KITT. 
-%
-% Version: 0.1 (26-05-2019)
+%    for KITT.
+% Version: 0.2 (27-05-2019)
 % Group: B.04
+
+% Arguments:
+%   startpoint: [x, y] location of startingpoint
+%   endpoint: [x, y] location of endpoint
+%   waypoint: [x, y] location of waypoint, if waypoint = 0: no waypoint
+%   obstacles: true/false
+
+
+
 
 end%KITTControl
