2abfff9169515d0be04366129b5045183282defa
Aron Hoogeveen <aron.hoogeveen@gmail.com>
2019-06-07 01:34:21 +0200

    Update for recursive example

--- matlab/final-challenge/recursiveFunc.m
+++ matlab/final-challenge/recursiveFunc.m
@@ -1,7 +1,7 @@
 function [res] = recursiveFunc(argin)
 if (argin ~= 3)
-    res = recursiveFunc(argin + 1);
     disp("Hoi ik ben een functie met argin waarde " + string(argin));
+    res = recursiveFunc(argin + 1);
 else
     disp("OMG IK BEN EINDELIJK AANGEROEPEN. argin = " + string(argin));
     res = argin;
