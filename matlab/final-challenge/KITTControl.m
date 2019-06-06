1e985fdf7f6d71fa0ac618c45f18f433493fbfa5
Aron Hoogeveen <aron.hoogeveen@gmail.com>
2019-06-06 21:08:10 +0200

    Small changes for KITTControl

--- matlab/final-challenge/KITTControl.m
+++ matlab/final-challenge/KITTControl.m
@@ -136,7 +136,7 @@ if (challengeA)% Challenge A: no waypoint
                 if(length(x_points) > floor(pointsAmount/2)) % Dit moet aangepast worden aan de hand van de lengte van het stuk met pointsAmount
                     % make a trend and calculate the angle
                     prms = polyfit(x_points,y_points,1);
-                    rico = prms(1);
+                    rico = prms(1); % Richtingscoëfficient
                     phi = atand(rico);
                     if (x_points(end)-x_points(1) < 0 && y_points(end)-y_points(1) < 0)
                         actual_orientation = phi - 180;
