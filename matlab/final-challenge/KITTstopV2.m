4126dbcc37642d19966020e45be7bfd55126d287
Aron Hoogeveen <aron.hoogeveen@gmail.com>
2019-06-07 02:27:40 +0200

    Update for KITTstopV2
    which fixes indexes that were not found

--- matlab/final-challenge/KITTstopV2.m
+++ matlab/final-challenge/KITTstopV2.m
@@ -16,19 +16,21 @@ function [timeToDrive, vPoint] = KITTstopV2(distance, x_brake, v_brake, x_acc, v
     v_acc = v_acc(iEndSpeed:end); % FIXME it is possible that the cropping causes the v_acc to be too short. If that is the case just trek het grafiekje door
     x_acc = x_acc(iEndSpeed:end);
     x_acc = x_acc - min(x_acc);
-    %figure;
-     %plot(x_acc);title('xacc');
-    %plot(x_acc, v_acc);title('x - v - acc zoals kittstopv2');
-    %figure;
+    figure;
+     plot(x_acc);title('xacc');
+     figure
+    plot(x_acc, v_acc);title('x - v - acc zoals kittstopv2');
+    figure;
     
     xPoint = CurvesIntersect(distance, x_brake, v_brake, x_acc, v_acc, brakeEnd, 1);
     vPoint = CurvesIntersect(distance, x_brake, v_brake, x_acc, v_acc, brakeEnd, 0);
 
     % Convert the xPoint to the time it takes to get to that point. 
-    i = find(abs(x_acc-xPoint) < 0.1); % Get the index of x_acc where the distance is equal to the stop distance.
-    %disp("xPoint is " + string(xPoint));
-%     disp("i is " + string(i));
-%     disp("x_acc = ");
-%     disp(x_acc);
+    [ ~, i ] = min( abs( x_acc-xPoint ) );
+%     i = find(abs(x_acc-xPoint) < 0.1); % Get the index of x_acc where the distance is equal to the stop distance.
+    disp("xPoint is " + string(xPoint));
+    disp("i is " + string(i));
+    disp("x_acc = ");
+    disp(x_acc);
     timeToDrive = i * 37; % 1 sample corresponds to 37 miliseconds. 
 end%KITTstopV2
